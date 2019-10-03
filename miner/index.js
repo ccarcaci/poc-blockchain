1d5f1e3a6d033d217109b94aaae2ff3be73c77f9
Claudio Carcaci <claudiocarcaci@gmail.com>
2019-10-03 20:55:44 +0200

    Add SHA3 library Use slice() to duplicate ledgerContent and get the last page Add some logging Fix string and buffer data formats Use setInterval as base execution for miner Remove tls unauthorized cert rejection Use url parse in POST Add uuiv4 to generate padding

--- miner/index.js
+++ miner/index.js
@@ -1,60 +1,69 @@
 "use strict"
 
 const https = require("https")
-
-const runMiner = async () => {
-  try {
-    let lifeSpan = 9999
-
-    while(--lifeSpan) {
-      // eslint-disable-next-line no-await-in-loop
-      const currentPageContent = await getCurrentPageContent()
-      const pageContent = addPadding(currentPageContent)
-
-      if(verifyProofOfWork(pageContent)) {
-        addPage(pageContent)
-      }
-    }
-  } catch(error) {
-    console.log(error.message)
-  }
-}
-
-runMiner()
-
-// Miner Functions
-
-const getCurrentPageContent = () => doGet("https://ledger-manager/current-page").pageContent
-const addPadding = (pageContent) => pageContent.proofOfWork.padding = uuidv4()
-const verifyProofOfWork = (pageContent) => sha3(pageContent).slice(0, 1) === "42"
-const addPage = (pageContent) => doPost("https://ledger-manager/add-page", pageContent)
-const uuidv4 = () => "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (chars) => {
-  const random = Math.random() * 16 | 0, value = chars === "x" ? random : (random & 0x3 | 0x8)
-  return value.toString(16)
-})
-const sha3 = (pageContent) => pageContent
+const { SHA3, } = require("sha3")
+const url = require("url")
+require("process").env.NODE_TLS_REJECT_UNAUTHORIZED = 0
 
 // Https Functions
 
-const doGet = (url) => new Promise((resolve, reject) => https.get(url, (response) => {
-  let data = ""
-  response.on("data", (chunk) => data += chunk)
-  response.on("end", resolve(data))
-}).on("error", (err) => reject(err)))
+const doGet = (url) => new Promise((resolve, reject) => {
+  const request = https.get(url, (response) => {
+    let data = ""
+    response.on("data", (chunk) => data += chunk)
+    response.on("end", () => resolve(JSON.parse(data)))
+  })
+  request.on("error", (err) => reject(err))
+  request.end()
+})
+const doPost = (completeUrl, content) => {
+  const jsonStringContent = JSON.stringify(content)
+  const action = url.parse(completeUrl)
 
-const doPost = (url, content) => {
   const options = {
-    hostname: url.split("/")[0],
+    hostname: action.hostname,
     port: 4443,
-    path: url.split("/")[1],
+    path: action.pathname,
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "Content-Length": content.length,
+      "Content-Length": jsonStringContent.length,
     },
   }
 
   const request = https.request(options)
-  request.write(content)
+  request.write(jsonStringContent)
   request.end()
 }
+
+// Miner Functions
+
+const addPadding = (pageContent) => pageContent.proofOfWork.padding = uuidv4()
+const verifyPOW = (pageContent) => sha3(pageContent).slice(0, 1) === "4"
+const addPage = (pageContent) => doPost("https://localhost:4443/add-page", pageContent)
+const uuidv4 = () => "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (chars) => {
+  const random = Math.random() * 16 | 0, value = chars === "x" ? random : (random & 0x3 | 0x8)
+  return value.toString(16)
+})
+const sha3 = (pageContent) => {
+  const hash = new SHA3(512)
+  hash.update(JSON.stringify(pageContent))
+  return hash.digest().toString("hex")
+}
+const getCurrentPageContent = async () => {
+  const page = await doGet("https://localhost:4443/current-page")
+  return page.pageContent
+}
+
+// Main
+
+setInterval(async () => {
+  try {
+    // eslint-disable-next-line no-await-in-loop
+    const currentPageContent = await getCurrentPageContent()
+    addPadding(currentPageContent)
+    verifyPOW(currentPageContent) && addPage(currentPageContent)
+  } catch (error) {
+    console.log(error.message)
+  }
+}, 200)
