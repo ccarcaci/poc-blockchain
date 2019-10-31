05e44fc2416b41a54247ef9e4384c9519b8390aa
Claudio Carcaci <claudiocarcaci@gmail.com>
2019-10-31 16:11:06 +0100

    Add propagation function

--- ledger-operator/nodes.manager.js
+++ ledger-operator/nodes.manager.js
@@ -8,16 +8,27 @@ let knownNodes = []
 module.exports = {
   Store: {
     save: (nodes) => knownNodes = nodes,
-    read: () => knownNodes,
+    load: () => knownNodes,
   },
   Pure: {
-    propagate: (sourceNode, newNodes, knownNodes) => {
-      return [ sourceNode, ...newNodes, ...knownNodes ]
+    join: (...nodes) => {
+      return [...new Set(nodes)]
+    },
+    merge: (sourceNode, newNodes, knownNodes) => {
+      return [...new Set([ sourceNode, ...newNodes, ...knownNodes ])]
+    },
+    unknowns: (sourceNode, newNodes, knownNodes) => {
+      return [...new Set([ sourceNode, ...newNodes ])].filter((node) => !knownNodes.includes(node))
     },
     initializeKnownNodes: (refNode, callback) => {
       doPost(refNode, [])
         .catch((error) => log.error(error))
         .then((knownNodes) => callback(knownNodes))
+    },
+    propagate: (nodes, callback) => {
+      nodes.forEach((node) => doPost(node, [])
+        .catch((error) => log.error(error))
+        .then((receivedNodes) => callback(receivedNodes)))
     },
   },
 }
