a9624426f4ac98e35c06a50520b421fca25a161c
Claudio Carcaci <claudiocarcaci@gmail.com>
2019-11-17 11:12:44 +0100

    Add tracker Add docker-compose

---
+++ tracker/index.js
@@ -0,0 +1,57 @@
+"use strict"
+
+const http = require("http")
+const https = require("https")
+const url = require("url")
+const fs = require("fs")
+
+const httpsOptions = {
+  key: fs.readFileSync("./certs/privkey.pem"),
+  cert: fs.readFileSync("./certs/certificate.crt"),
+}
+
+const httpPort = 3000
+const httpsPort = 4443
+
+const routing = (request, response) => {
+  const action = url.parse(request.url)
+
+  if(action.pathname === "/") { rootRoute(response) }
+  else if(request.method === "GET" && action.pathname === "/track") { track(request, response) }
+  else { fallbackRoute(response) }
+}
+
+const httpServer = http.createServer((req, res) => routing(req, res))
+const httpsServer = https.createServer(httpsOptions, (req, res) => routing(req, res))
+
+httpServer.listen(httpPort, "0.0.0.0", () => console.log(`HTTP Server on port ${httpPort}`))
+httpsServer.listen(httpsPort, "0.0.0.0", () => console.log(`HTTPS Server on port ${httpsPort}`))
+
+// Routing Functions
+
+const rootRoute = (response) => {
+  response.writeHead(200, { "Content-Type": "text/plain" })
+  response.write("Try with /track")
+  response.end()
+}
+const track = (request, response) => {
+  const remoteAddress = request.socket.remoteAddress
+
+  operators = storeOperators(remoteAddress)
+
+  response.writeHead(200, { "Content-Type": "application/json" })
+  response.write(JSON.stringify({ operators }))
+  response.end()
+}
+const fallbackRoute = (response) => {
+  response.writeHead(404)
+  response.end()
+}
+
+// Server Functions
+
+const storeOperators = (operatorAddress) => operators.length === 0 ?
+  [operatorAddress]
+  : [ operators.pop(), operatorAddress ]
+
+let operators = []
