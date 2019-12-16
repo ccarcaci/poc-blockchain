ea7e39c12400f93c51031f3b5600855758221419
Claudio Carcaci <claudiocarcaci@gmail.com>
2019-12-16 17:23:51 +0100

    Implementation of single node chain in progress

--- ledger/src/index.js
+++ ledger/src/index.js
@@ -23,19 +23,17 @@ const routing = (request, response) => {
   const action = url.parse(request.url)
 
   if(action.pathname === "/") { rootRoute(response) }
-  else if(request.method === "POST" && action.pathname === "/new-nodes") { addNodesAndPropagate(request, response) }
+  else if(request.method === "GET" && action.pathname === "/chain") { getChain(response) }
+  else if(request.method === "POST" && action.pathname === "/transaction") { addTransaction(request, response) }
+  else if(request.method === "GET" && action.pathname === "/current-page") { getCurrentPage(response) }
   else { fallbackRoute(response) }
 }
 
-nodesManager.Operations.initializeKnownNodes(config.refNodeUrl, (knownNodes) => {
-  nodesManager.Store.save(knownNodes.operators)
-  logger.info(nodesManager.Store.load())
-  const httpServer = http.createServer((req, res) => routing(req, res))
-  const httpsServer = https.createServer(httpsOptions, (req, res) => routing(req, res))
+const httpServer = http.createServer((req, res) => routing(req, res))
+const httpsServer = https.createServer(httpsOptions, (req, res) => routing(req, res))
 
-  httpServer.listen(httpPort, () => logger.info(`HTTP Server on port ${httpPort}`))
-  httpsServer.listen(httpsPort, () => logger.info(`HTTPS Server on port ${httpsPort}`))
-})
+httpServer.listen(httpPort, () => logger.info(`HTTP Server on port ${httpPort}`))
+httpsServer.listen(httpsPort, () => logger.info(`HTTPS Server on port ${httpsPort}`))
 
 // Routing Functions
 
@@ -47,23 +45,17 @@ const fallbackRoute = (response) => {
   response.writeHead(404)
   response.end()
 }
-const addNodesAndPropagate = (request, response) => {
-  const knownNodes = nodesManager.Store.load()
-
-  response.writeHead(200, { "Content-Type": "application/json" })
-  response.write(JSON.stringify(knownNodes))
+const getChain = (response) => {
+  response.write(chain.full())
+  response.writeHead(200)
   response.end()
+}
+const addTransaction = (request, response) => {
+  const transaction = getBody(request)
+  chain.addTransaction(transaction)
 
-  getBody(request)
-    .then((newNodes) => {
-      const unknownNodes = nodesManager.Operations.unknowns(request.url, newNodes, knownNodes)
-
-      if(unknownNodes.size <= 0) { return }
-
-      const allNodes = nodesManager.Operations.merge(request.url, newNodes, knownNodes)
-      nodesManager.Operations.propagate(allNodes, (receivedNodes) => nodesManager.Store.save(nodesManager.Operations.join(receivedNodes, allNodes)))
-    })
-    .catch((error) => logger.error(error))
+  response.writeHead(200)
+  response.end()
 }
 
 // Server Functions
@@ -73,3 +65,7 @@ const getBody = (request) => new Promise((resolve) => {
   request.on("data", (chunk) => body+=chunk)
   request.on("end", () => resolve(JSON.parse(body)))
 })
+
+// Mining
+
+setInterval(() => chain.mine(), interval)
