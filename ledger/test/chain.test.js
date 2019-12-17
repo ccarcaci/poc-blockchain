6cc8410fa090616e90d5d7e6515b20a5be77cfbc
Claudio Carcaci <claudiocarcaci@gmail.com>
2019-12-17 15:47:24 +0100

    Fix addPage function Add some tests on chain management

--- ledger/test/chain.test.js
+++ ledger/test/chain.test.js
@@ -1,9 +1,9 @@
 "use strict"
 
-const chain = require("../src/chain")
-
 describe("Chain Functionalities", () => {
+  afterEach(() => jest.resetModules())
   test("Create new chain with an empty page", () => {
+    const chain = require("../src/chain")
     expect(chain.full()).toEqual([{
       pageContent: {
         transactions: [ ],
@@ -12,6 +12,51 @@ describe("Chain Functionalities", () => {
       },
       pageHash: "",
     }])
+  })
+  test("Add transaction to chain", () => {
+    const chain = require("../src/chain")
+    chain.addTransaction({ eenie: "meenie" })
+
+    expect(chain.full()).toEqual([{
+      pageContent: {
+        transactions: [{ eenie: "meenie" }],
+        previousPageHash: "",
+        padding: "",
+      },
+      pageHash: "",
+    }])
+  })
+  test("Mine new page", () => {
+    const chain = require("../src/chain")
+    const crypto = require("../src/crypto")
+    jest.mock("../src/crypto.js")
+
+    chain.addTransaction({ eenie: "meenie" })
+
+    crypto.uuidv4.mockReturnValue("xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx")
+    crypto.sha3.mockReturnValue("42")
+    crypto.verifyPOW.mockReturnValue(true)
+
+    chain.mine()
+
+    expect(chain.full()).toEqual([
+      {
+        pageContent: {
+          transactions: [{ eenie: "meenie" }],
+          previousPageHash: "",
+          padding: "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx",
+        },
+        pageHash: "42",
+      },
+      {
+        pageContent: {
+          transactions: [ ],
+          previousPageHash: "42",
+          padding: "",
+        },
+        pageHash: "",
+      },
+    ])
   })
 })
 
